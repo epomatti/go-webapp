1914f652ef67212e35d7f7a64c3ec380f9547bdc
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-04-29 20:46:36 -0300

    api endpoint

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
+
+	"main/internal/data"
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -21,23 +24,69 @@ func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 
 	js, err := json.Marshal(data)
 	if err != nil {
-		serverError := http.StatusInternalServerError
-		http.Error(w, http.StatusText(serverError), serverError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
+
 	w.Write(js)
 }
 
 func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		fmt.Fprintln(w, "Display a list of the books on the reading list")
-		return
+		books := []data.Book{
+			{
+				ID:        1,
+				CreatedAt: time.Now(),
+				Title:     "The Darkening of Tristram",
+				Published: 1998,
+				Pages:     300,
+				Genres:    []string{"Fiction", "Thriller"},
+				Rating:    4.5,
+				Version:   1,
+			},
+			{
+				ID:        2,
+				CreatedAt: time.Now(),
+				Title:     "The Legecy of Deckard Cain",
+				Published: 2007,
+				Pages:     432,
+				Genres:    []string{"Fiction", "Adventure"},
+				Rating:    4.9,
+				Version:   1,
+			},
+			{
+				ID:        3,          // system generated
+				CreatedAt: time.Now(), // system generated
+				Title:     "The Black Soulstone",
+				Version:   1, // system generated
+			},
+		}
+
+		if err := app.writeJSON(w, http.StatusOK, envelope{"books": books}); err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
 	}
 	if r.Method == http.MethodPost {
-		fmt.Fprintln(w, "Added a new book to the reading list")
-		return
+		var input struct {
+			Title     string   `json:"title"`
+			Published int      `json:"published"`
+			Pages     int      `json:"pages"`
+			Genres    []string `json:"genres"`
+			Rating    float32  `json:"rating"`
+		}
+
+		err := app.readJSON(w, r, &input)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "%+v\n", input)
 	}
 }
 
@@ -61,7 +110,22 @@ func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Display the details of a specific book with ID: %d\n", idInt)
+
+	book := data.Book{
+		ID:        idInt,
+		CreatedAt: time.Now(),
+		Title:     "Echoes in the Darkness",
+		Published: 2019,
+		Pages:     300,
+		Genres:    []string{"Fiction", "Thriller"},
+		Rating:    4.5,
+		Version:   1,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"book": book}); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
@@ -71,7 +135,56 @@ func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Update the details of a specific book with ID: %d\n", idInt)
+
+	var input struct {
+		Title     *string  `json:"title"`
+		Published *int     `json:"published"`
+		Pages     *int     `json:"pages"`
+		Genres    []string `json:"genres"`
+		Rating    *float32 `json:"rating"`
+	}
+
+	book := data.Book{
+		ID:        idInt,
+		CreatedAt: time.Now(),
+		Title:     "Echoes in the Darkness",
+		Published: 2019,
+		Pages:     300,
+		Genres:    []string{"Fiction", "Thriller"},
+		Rating:    4.5,
+		Version:   1,
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	if input.Title != nil {
+		book.Title = *input.Title
+	}
+
+	if input.Published != nil {
+		book.Published = *input.Published
+	}
+
+	if input.Pages != nil {
+		book.Pages = *input.Pages
+	}
+
+	if len(input.Genres) > 0 {
+		book.Genres = input.Genres
+	}
+
+	if input.Rating != nil {
+		book.Rating = *input.Rating
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"book": book}); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
@@ -81,5 +194,5 @@ func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Delete a specific book with ID: %d\n", idInt)
+	fmt.Fprintf(w, "Delete a specific book with ID: %d", idInt)
 }
